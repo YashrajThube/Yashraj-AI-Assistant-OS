87e71407b0fdf4feb998da06349d642e4d236bc2
Yashraj Thube <yashraj07thube@gmail.com>
2026-06-16 09:26:03 +0530

    Cleanup repository

--- backup_pre_cleanup/backend/app.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-
-from app.core.config import API_HOST, API_PORT
-from app.main import app
-
-
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host=API_HOST, port=API_PORT, reload=False)
